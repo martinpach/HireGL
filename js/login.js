1b34e5a1bfab402ab79f0d8655f48f0bc466b53f
martin <martin.pach@akademiasovy.sk>
2017-02-06 17:44:02 +0100

    Data about logged user sent using local storage

--- js/login.js
+++ js/login.js
@@ -14,13 +14,16 @@
              data: JSON.stringify({ "userName": userName, "password": password }),
              success: function(data) {
                  var user = data.user;
-                 console.log(data.token);
-                 window.location.href = 'index.html?firstName=' + user.firstName + '&lastName=' + user.lastName + '&photoUrl=' + user.photoUrl; //redirect to index
+                 // creating local storage items with user data that will be sent to main.js
+                 localStorage.setItem("firstName", user.firstName);
+                 localStorage.setItem("lastName", user.lastName);
+                 localStorage.setItem("photoUrl", user.photoUrl);                
+                 window.location.href = 'index.html';
              },
              error: function() {
                  errorField.show();
              }
          });
      });
-     /* /login request */
+     /* END of login request */
  });
--- js/main.js
+++ js/main.js
@@ -1,15 +1,11 @@
 $(document).ready(function() {
 
-	/* retrieving data from url and load user information */
-    $.urlParam = function(name) {
-        var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-        return results[1] || 0;
-    }
+	/* retrieving data from local storage and load user information */
 
     var data = {
-    	firstName : $.urlParam('firstName'),
-    	lastName : $.urlParam('lastName'),
-    	photoUrl : $.urlParam('photoUrl')
+        firstName : localStorage.getItem("firstName"),
+        lastName : localStorage.getItem("lastName"),
+        photoUrl : localStorage.getItem("photoUrl")  
     };
 
     var userDataWrapper = '<div id="userData"><span id="v-align"><img src="{{photoUrl}}" id="user-icon">{{firstName}} {{lastName}}</span><i class="material-icons" id="logout">arrow_forward</i></div>';
@@ -19,5 +15,5 @@ $(document).ready(function() {
     $('#user-account-wrapper').html(html);
     $('#user-account-wrapper-r').html(htmlResponsive);
 
-    /* /retrieving data from url and load user information */
+    /* END of retrieving data from local storage and load user information */
 });
