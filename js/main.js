9f0eb84d1403d5343efcb54749b3c1d2b04ec485
martin <martin.pach@akademiasovy.sk>
2017-02-07 14:24:27 +0100

    Logout done

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
 
-	/* retrieving data from local storage and load user information */
+    /* retrieving data from local storage and load user information */
 
     var data = {
-        firstName : localStorage.getItem("firstName"),
-        lastName : localStorage.getItem("lastName"),
-        photoUrl : localStorage.getItem("photoUrl")  
+        firstName: localStorage.getItem("firstName"),
+        lastName: localStorage.getItem("lastName"),
+        photoUrl: localStorage.getItem("photoUrl")
     };
+
+    var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE0ODE2MzU2ODksInN1YiI6InVzZXIxIn0.urh7CmQmz4c03dVq5qoFyFwXypTqW_95x9uBtvoKrVY9IVDeMXBjK2nCp_R9hXgPKuR_ggdqUhZPMu7uSZlUeA";
 
     var userDataWrapper = '<div id="userData"><span id="v-align"><img src="{{photoUrl}}" id="user-icon">{{firstName}} {{lastName}}</span><i class="material-icons" id="logout">arrow_forward</i></div>';
     var html = Mustache.to_html(userDataWrapper, data);
@@ -16,4 +18,21 @@ $(document).ready(function() {
     $('#user-account-wrapper-r').html(htmlResponsive);
 
     /* END of retrieving data from local storage and load user information */
+
+
+    /* event handlers */
+
+    /* logout after clicking arrow button */
+    $('#user-account-wrapper, #user-account-wrapper-r').on('click', 'i', function() {
+        $.ajax({
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+            },
+            url: 'http://localhost:8081/api/auth/logout',
+            type: 'POST',
+            success : function () {
+                window.location.href = 'login.html';
+            }
+        });
+    });
 });
