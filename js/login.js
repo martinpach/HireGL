446892ebcf2131367cd38861b2285e2b6254b2a7
martin <martin.pach@akademiasovy.sk>
2017-02-09 09:56:46 +0100

    Login request fired after all mandatory fields are filled

--- js/login.js
+++ js/login.js
@@ -1,31 +1,39 @@
- $(document).ready(function () {
+ $(document).ready(function() {
      /* login request */
-     $('#login-btn').on('click', function (event) {
+     $('#login-btn').on('click', function(event) {
          var userName = $('#user-name').val();
          var password = $('#password').val();
          var errorField = $('#error-field');
          var link = $(this).children();
-         $.ajax({
-             url: 'http://localhost:8081/api/auth/login'
-             , type: 'POST'
-             , contentType: 'application/json'
-             , data: JSON.stringify({
-                 "userName": userName
-                 , "password": password
-             })
-             , success: function (data) {
-                 var user = data.user;
-                 // creating local storage items with user data that will be sent to main.js
-                 localStorage.setItem("firstName", user.firstName);
-                 localStorage.setItem("lastName", user.lastName);
-                 localStorage.setItem("photoUrl", user.photoUrl);
-                 localStorage.setItem("token", data.token);
-                 window.location.href = 'main.html';
-             }
-             , error: function () {
-                 errorField.show();
-             }
-         });
+
+         if (userName && password) {
+
+             $.ajax({
+                 url: 'http://localhost:8081/api/auth/login',
+                 type: 'POST',
+                 contentType: 'application/json',
+                 data: JSON.stringify({
+                     "userName": userName,
+                     "password": password
+                 }),
+                 success: function(data) {
+                     var user = data.user;
+                     // creating local storage items with user data that will be sent to main.js
+                     localStorage.setItem("firstName", user.firstName);
+                     localStorage.setItem("lastName", user.lastName);
+                     localStorage.setItem("photoUrl", user.photoUrl);
+                     localStorage.setItem("token", data.token);
+                     window.location.href = 'main.html';
+                 },
+                 error: function() {
+                     errorField.show();
+                 }
+             });
+         }
+
+         else{
+         	errorField.show();
+         }
      });
      /* END of login request */
  });
