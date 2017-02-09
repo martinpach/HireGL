77ed94d327bf7156c8fcba470c657fb9bfe9aca0
wdfeww <wdfeww@gmail.com>
2017-02-09 16:12:21 +0100

    now you can login by pressing enter

--- js/login.js
+++ js/login.js
@@ -1,22 +1,20 @@
- $(document).ready(function() {
+ $(document).ready(function () {
      /* login request */
-     $('#login-btn').on('click', function(event) {
+     function login() {
          var userName = $('#user-name').val();
          var password = $('#password').val();
          var errorField = $('#error-field');
          var link = $(this).children();
-
          if (userName && password) {
-
              $.ajax({
-                 url: 'http://localhost:8081/api/auth/login',
-                 type: 'POST',
-                 contentType: 'application/json',
-                 data: JSON.stringify({
-                     "userName": userName,
-                     "password": password
-                 }),
-                 success: function(data) {
+                 url: 'http://localhost:8081/api/auth/login'
+                 , type: 'POST'
+                 , contentType: 'application/json'
+                 , data: JSON.stringify({
+                     "userName": userName
+                     , "password": password
+                 })
+                 , success: function (data) {
                      var user = data.user;
                      // creating local storage items with user data that will be sent to main.js
                      localStorage.setItem("firstName", user.firstName);
@@ -24,16 +22,23 @@
                      localStorage.setItem("photoUrl", user.photoUrl);
                      localStorage.setItem("token", data.token);
                      window.location.href = 'main.html';
-                 },
-                 error: function() {
+                 }
+                 , error: function () {
                      errorField.show();
                  }
              });
          }
-
-         else{
-         	errorField.show();
+         else {
+             errorField.show();
          }
+     }
+     $(document).keydown(function (event) {
+         if (event.keyCode == 13) {
+             login();
+         }
+     });
+     $('#login-btn').on('click', function (event) {
+         login();
      });
      /* END of login request */
  });
