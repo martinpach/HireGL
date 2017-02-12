36ccbd021051fccbff84b7255a3bde8a4ba6a06c
wdfeww <wdfeww@gmail.com>
2017-02-12 15:16:50 +0100

    fixed count of displayed interviews

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 $(document).ready(function () {
     var token = localStorage.getItem("token");
-    /* loading templates */
-    $('#main-content').load('../templates/my-interviews.html', function () {
-        getInterviews(1, 5);
-    });
     /*on click to new interview*/
     $('#menu-new-interview , #new-interview-r').on('click', function () {
         /*changing main content to new interview form and getting from server positons, locations and rooms*/
@@ -258,11 +254,10 @@ $(document).ready(function () {
             notEmpty = 0;
             fieldWrongInput("#new-int-assperson", "Please choose one option");
             $('#new-int-assperson + div.wrong-input').show();
-        } 
+        }
         if (notEmpty == 0) {
             return false;
-        }
-        else {
+        } else {
             return true;
         }
     }
@@ -493,12 +488,8 @@ $(document).ready(function () {
             "class": 'material-icons',
             "id": 'delete'
         }).text("delete").appendTo("#editInterview");
-        $("#delete").on('mouseenter', function () {
-            $(this).text("delete_forever");
-        });
-        $("#delete").on('mouseleave', function () {
-            $(this).text("delete");
-        });
+
+
         $("#icoDisableRight").on('click', function () {
             mui.overlay('off');
         });
